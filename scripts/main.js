20ff9e5320f23c10b1d97cb8a805c74a8f7aeb20
佐久間隆斗 <sakumatakashito@sakumatakashitononotobukkukonpyuta.local>
2024-06-22 15:55:42 +0900

    おみくじボタンを押すとランダムで他のページに飛ぶようにしました

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('button.btn-primary');
-    
-    button.addEventListener('click', function() {
-      alert('ボタンがクリックされました！');
-    });
-  });
+class Omikuji{
+    constructor() {
+        this.html = this.getHtml(getRandomNumber(3)); 
+    }
+    getHtml(number) {
+        let html = '';
+        if (number < 1) html = 'goodFortune.html';
+        else if (number >= 1 && number < 2) html = 'lucky.html';
+        else html = 'badLuck.html';
+        return html;
+      }
+}
+const omikuji = new Omikuji();
+
+function redirectToNewPage() {
+    window.location.href = omikuji.html;
+}
+
+// 0~maxまでのランダムなを返す
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
